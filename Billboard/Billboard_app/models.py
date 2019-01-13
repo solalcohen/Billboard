8e1ba0cd5dad2cc1faf01a9423dc1ae7c8aaa71e
solalcohen <solalcohen@icloud.com>
2019-01-13 02:36:56 +0200

    fix responsiveness and date

--- Billboard/Billboard_app/models.py
+++ Billboard/Billboard_app/models.py
@@ -3,13 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django import forms
+import time
+
+date_created = time.strftime("%Y/%m/%d")
 
 
 class Billboard(models.Model):
-    title = models.CharField(max_length=50,null=False)
-    text = models.CharField(max_length=300,null=False)
-    author = models.CharField(max_length=10,null=False)
+    title = models.CharField(max_length=50, null=False)
+    text = models.CharField(max_length=300, null=False)
+    author = models.CharField(max_length=10, null=False)
+    date = models.CharField(max_length=20, default=date_created)
 
     def __str__(self):
         return self.title
-
--- Billboard/Billboard_app/views.py
+++ Billboard/Billboard_app/views.py
@@ -2,16 +2,18 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from .models import Billboard
+import time
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def index(request):
+    date_post = time.strftime("%Y/%m/%d")
     Billboards = Billboard.objects.all()
     if request.method == 'POST':
         title = request.POST.get('title')
         text = request.POST.get('text')
         author = request.POST.get('author')
-        model = Billboard(title=title, text=text, author=author)
+        model = Billboard(title=title, text=text, author=author,date=date_post)
         model.save()
-    return render(request, 'Billboard_app/index.html', {'Billboards': Billboards})
+    return render(request, 'Billboard_app/index.html', {'Billboards': Billboards,'date':date_post})
